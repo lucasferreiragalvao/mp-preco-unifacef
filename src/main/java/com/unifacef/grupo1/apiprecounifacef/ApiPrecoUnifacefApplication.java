bf47e5e2a2553469018d15704347596557575afa
Lucas Galvão <lucasferreiragalvaoti@gmail.com>
2021-11-15 15:37:48 -0300

    commit inicial

---
+++ src/main/java/com/unifacef/grupo1/apiprecounifacef/ApiPrecoUnifacefApplication.java
@@ -0,0 +1,13 @@
+package com.unifacef.grupo1.apiprecounifacef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPrecoUnifacefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPrecoUnifacefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unifacef/grupo1/apiprecounifacef/ApiPrecoUnifacefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifacef.grupo1.apiprecounifacef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPrecoUnifacefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
